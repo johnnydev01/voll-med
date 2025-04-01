db523e8f7db3efdd2402a89dc7b228daae666592
Johnny Silva <johnnysilva.dev@gmail.com>
2025-04-01 16:04:02 -0300

    feat: controlando o acesso das requisições

--- src/main/java/med/voll/api/services/TokenService.java
+++ src/main/java/med/voll/api/services/TokenService.java
@@ -4,6 +4,7 @@ package med.voll.api.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.models.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
                     .sign(algoritimo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubjet(String tokenJWT) {
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+           return JWT.require(algoritimo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                   .verify(tokenJWT)
+                   .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
